d6d23fddc2e4fa63057592e9bd0ccf8a2e3a565a
kriss <462679766@qq.com>
2025-06-05 10:13:05 +0800

    feature: add Logger tests

--- src/Processors/RequestUidProcessor.php
+++ src/Processors/RequestUidProcessor.php
@@ -3,8 +3,8 @@
 namespace WebmanTech\Logger\Processors;
 
 use Monolog\LogRecord;
-use WebmanTech\Logger\Middleware\RequestUid;
 use Monolog\Processor\UidProcessor;
+use WebmanTech\Logger\Middleware\RequestUid;
 
 /**
  * 网站请求唯一标识记录，需要配合 Middleware\RequestUid
@@ -22,12 +22,14 @@ class RequestUidProcessor extends UidProcessor
      */
     public function __invoke(LogRecord $record): LogRecord
     {
-        $uid = null;
-        if ($request = request()) {
-            $uid = $request->{RequestUid::REQUEST_UID_KEY};
-        }
+        if (!isset($record->extra['uid'])) {
+            $uid = null;
+            if ($request = request()) {
+                $uid = $request->{RequestUid::REQUEST_UID_KEY};
+            }
 
-        $record->extra['uid'] = $uid ?: $this->getUid();
+            $record->extra['uid'] = $uid ?: $this->getUid();
+        }
 
         return $record;
     }
